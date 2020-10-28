6879b95c59dbe6f66572a59ddea81cf0edce5706
tpk73 <tpaaka73@gmail.com>
2020-10-27 20:35:08 -0500

    Almost done with class episode

--- SwiftyIRDB_TPK/IRDb_TableViewController.swift
+++ SwiftyIRDB_TPK/IRDb_TableViewController.swift
@@ -8,12 +8,7 @@
 import UIKit
 
 class IRDb_TableViewController: UITableViewController {
-    
-    //**** DELETE GARBAGE LATER ****////
-    var outerArray = ["First section name", "Second section name"]
-    var innerArray1 = ["frist thing", "second thing", "third thing"]
-    var innerArray2 = ["frist thing", "second thing", "third thing", "fourth thing"]
-    
+
     var mediaModel: IRDb_MediaDataModel? {
         didSet {
             tableView.reloadData()
@@ -54,36 +49,26 @@ class IRDb_TableViewController: UITableViewController {
     // MARK: - Table view data source
 
     override func numberOfSections(in tableView: UITableView) -> Int {
-        return outerArray.count
+        return mediaModel?.franchise.count ?? 0
     }
     
     override func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
-        return outerArray[section]
+        return mediaModel?.franchise[section].franchiseName
     }
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         
-        if (section == 0){
-            return innerArray1.count
-        }else if (section == 1){
-            return innerArray2.count
-        }else{
-            return 1
-        }
+        return mediaModel?.franchise[section].entries.count ?? 0
+        
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "mediaCell", for: indexPath)
 
-        var arrayForThisCell = [Any]()
-        if (indexPath.section == 0){
-            arrayForThisCell = innerArray1
-        }else if (indexPath.section == 1){
-            arrayForThisCell = innerArray2
-        }
-        
         // Configure the cell...
-        cell.textLabel?.text = arrayForThisCell[indexPath.row] as? String
+        cell.textLabel?.text = mediaModel?.franchise[indexPath.section].entries[indexPath.row].name
+        
+        cell.detailTextLabel?.text = mediaModel?.franchise[indexPath.section].entries[indexPath.row].yearStart
         
         return cell
     }
@@ -94,5 +79,12 @@ class IRDb_TableViewController: UITableViewController {
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         // Get the new view controller using segue.destination.
         // Pass the selected object to the new view controller.
+        if segue.identifier == "showMediaDetail" {
+            if let indexPath = tableView.indexPathForSelectedRow {
+                let selectedObject = mediaModel!.franchise[indexPath.section].entries[indexPath.row]
+                let controller = segue.destination as! detailViewController
+                controller.detailItem = selectedObject
+            }
+        }
     }
 }
---
+++ SwiftyIRDB_TPK/detailViewController.swift
@@ -0,0 +1,101 @@
+//
+//  ViewController.swift
+//  SwiftyIRDB_TPK
+//
+//  Created by Tanner Parker on 10/20/20.
+//
+
+import UIKit
+
+class detailViewController: UIViewController {
+    
+    
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var yearLabel: UILabel!
+    @IBOutlet weak var formatLabel: UILabel!
+    @IBOutlet weak var episodeLabel: UILabel!
+    @IBOutlet weak var studioNetworkLabel: UILabel!
+    @IBOutlet weak var posterImageView: UIImageView!
+    @IBOutlet weak var descriptionLabel: UILabel!
+    @IBOutlet weak var summaryTextView: UITextView!
+    @IBOutlet weak var castListButton: UIButton!
+    
+    func configureView() {
+        if let detail = detailItem {
+            if let label = titleLabel {
+                label.text = detail.name
+            }
+            title = detail.name
+            if let label = yearLabel {
+                if let ended = detail.yearEnd {
+                    label.text = "\(detail.yearStart) - \(ended)"
+                } else {
+                    label.text = detail.yearStart
+                }
+            }
+            if let label = formatLabel {
+                label.text = detail.format
+            }
+            if let label = episodeLabel {
+                if let countEpisodes = detail.episodes {
+                    if countEpisodes > 1 {
+                        label.text = "\(countEpisodes) Episodes"
+                    } else {
+                        label.text = "\(countEpisodes) Episodes"
+                    }
+                } else {
+                    label.text = ""
+                }
+            }
+            if let label = studioNetworkLabel {
+                if let network = detail.network {
+                    label.text = network
+                } else if let studio = detail.studio {
+                    label.text = studio
+                }
+            }
+            if let textview = summaryTextView {
+                textview.text = detail.summary
+            }
+            if let imageView = posterImageView {
+                let url = URL(string: detail.imageURL)
+                let data = try? Data(contentsOf: url!)
+                imageView.image = UIImage(data: data!)
+            }
+        }
+    }
+    
+    var detailItem: Entry? {
+        didSet{
+            configureView()
+        }
+    }
+
+    override func viewDidLoad() {
+        
+        super.viewDidLoad()
+        
+        // Do any additional setup after loading the view.
+        
+        // 1
+         let nav = self.navigationController?.navigationBar
+       
+         // 2
+         nav?.barStyle = UIBarStyle.black
+         nav?.tintColor = UIColor.init(red: 245/255, green: 196/255, blue: 72/255, alpha: 1)
+        
+    }
+    
+//    override func prepare (for segue: UIStoryboard, sender: Any?) {
+//        if segue.identifier == "showMediaDetail" {
+//            let controller = (segue.destination as! UINavigationController).topViewController as! CastListTableViewController
+//            if let castList = detailItem?.starring {
+//                controller.castList = castlist
+//            }
+//        }
+//    }
+
+
+
+}
+
